b7338b17b25a90c9c1e95fd95328e2d535b19d26
Igor Gabiatti <igornoro12@gmail.com>
2024-06-25 15:04:11 -0300

    primeiro commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title', 'Seja Bem-vindo')
+@section('conteudo')
+
+<div class="col-md-12  mx-auto justify-content-center align-items-center flex-column">
+        <p style="text-align: justify;">Sistema Finaceiro para gestão de contabilidade e custo de uma empresa.</p><br>
+</div>
+
+
+
+@endsection
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="text-center text-lg-start bg-dark fixed-bottom">
+    <!-- Copyright -->
+    <div class="text-center text-white p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        
+    </div>
+    <!-- Copyright -->
+</footer>
